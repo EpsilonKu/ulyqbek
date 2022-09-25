8d399ee1448bb26e7cb183ebe93177684e443614
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2022-09-25 01:40:59 -0600

    Add basic Table

---
+++ src/main/java/kz/bitter/ulyqbek/controller/MainController.java
@@ -0,0 +1,16 @@
+package kz.bitter.ulyqbek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kz.bitter.ulyqbek.model.Users;
+
+@Controller
+public class MainController {
+  @GetMapping( value = "/")
+  public String index (Model model){
+    Users users = new Users(null, "Nurda");
+    return "index";
+  }
+}
---
+++ src/main/java/kz/bitter/ulyqbek/model/Users.java
@@ -0,0 +1,26 @@
+package kz.bitter.ulyqbek.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "t_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String name;
+}
